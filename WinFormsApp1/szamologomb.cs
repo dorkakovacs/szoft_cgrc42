19ea41c290c7111b36863a905fe831d8985c11e7
dorkakovacs <kovacsdorka04@gmail.com>
2024-02-29 16:54:36 +0100

    har,adik gyakorlat

---
+++ WinFormsApp1/szamologomb.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harmaidkhet
+{
+    internal class szamologomb : villogogomb
+    {
+        int szám = 1;
+        public szamologomb()
+        {
+            Width = 20;
+            Height = 20;
+            MouseClick += Szamologomb_MouseClick;
+            int szám = 1;
+            Text = szám.ToString();
+        }
+
+        private void Szamologomb_MouseClick(object? sender, MouseEventArgs e)
+        {
+            szám++;
+            if (szám == 6) 
+            {
+                szám = 1;
+            }
+            Text = szám.ToString();
+        }
+    }
+}
---
+++ WinFormsApp1/villogogomb.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harmaidkhet
+{
+    internal class villogogomb : Button
+    {
+        public villogogomb() 
+        {
+            MouseEnter += Villogogomb_MouseEnter;
+            MouseLeave += Villogogomb_MouseLeave;
+        }
+
+        private void Villogogomb_MouseLeave(object? sender, EventArgs e)
+        {
+            BackColor = Color.White;
+        }
+
+        private void Villogogomb_MouseEnter(object? sender, EventArgs e)
+        {
+            BackColor = Color.Fuchsia;
+        }
+
+    }
+}
